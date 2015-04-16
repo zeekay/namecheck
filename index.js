d61533e6f8295045184f420eb37b20dced13e3c0
zeekay <zk@monoid.io>
2015-04-16 01:44:34 -0500

    Use request to handle redirects.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-var https = require('https'),
-    name = process.argv.slice(2)[0];
+var request = require('request');
+
+// Process args
+var name = process.argv.slice(2)[0];
 
 if (name == null) {
   console.log('what name should I search for?');
   process.exit(1);
 }
 
-https.get('https://www.npmjs.org/package/' + name, function(res) {
+request('https://www.npmjs.org/package/' + name, function(err, res) {
   if (res.statusCode === 200) {
     console.log(name + ' is unavailable: https://www.npmjs.org/package/' + name);
   } else if (res.statusCode === 404) {
