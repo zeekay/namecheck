f269b76d02bd070f58b5ebf5a23a15445f8f1d18
Gabriel Lebec <glebec@gmail.com>
2015-07-13 16:51:11 -0400

    checking multiple names

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 var request = require('request');
 
 // Process args
-var name = process.argv.slice(2)[0];
+var names = process.argv.slice(2);
 
-if (name == null) {
-  console.log('what name should I search for?');
+if (!names.length) {
+  console.log('what name(s) should I search for?');
   process.exit(1);
 }
 
-request('https://registry.npmjs.org/' + name, function(err, res) {
-  if (res.statusCode === 200) {
-    console.log(name + ' is unavailable: https://www.npmjs.org/package/' + name);
-  } else if (res.statusCode === 404) {
-    console.log(name + ' is available!');
-  } else {
-    console.log('something went wrong!');
-    process.exit(1)
-  }
-  process.exit(0)
+var checked = 0;
+names.forEach(function(name){
+  request('https://registry.npmjs.org/' + name, function(err, res) {
+    checked++;
+    if (res.statusCode === 200) {
+      console.log(name + ' is unavailable: https://www.npmjs.org/package/' + name);
+    } else if (res.statusCode === 404) {
+      console.log(name + ' is available!');
+    } else {
+      console.log('something went wrong checking', name);
+      if (err) console.error(err);
+    }
+    if (checked === names.length) process.exit(0);
+  });
 });
+
+var TIME = 3000;
+setTimeout(function(){
+  console.error('Timed out after', TIME / 1000, 'seconds' );
+  process.exit(1);
+}, TIME);
